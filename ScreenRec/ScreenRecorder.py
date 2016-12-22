2ae2196a69610e3f39f3a32cfad4b548e8bc51ad
Johannes Schriewer <j.schriewer@anfe.ma>
2016-12-22 18:37:10 +0100

    Add screen recording support for mac OS, add command line interface to screen recorder

--- ScreenRec/ScreenRecorder.py
+++ ScreenRec/ScreenRecorder.py
@@ -1,4 +1,6 @@
-import os, sys, subprocess
+import argparse
+import os, subprocess
+import platform
 
 # gi is GObject instrospection
 import gi
@@ -9,17 +11,26 @@ gi.require_version('Gst', '1.0')
 # Import GStreamer
 from gi.repository import Gst, GObject
 
+if platform.system() == 'Linux':
+    available_encoders = [
+        'software', # Uses libx264 'veryfast' preset, needs much CPU power
+        'vaapi', # Intel CPU driver (only when running Xorg on Intel or Glamour drivers
+        'nvenc', # NVidia encoder, needs GTX680 or higher (GK104/Keppler or higher) and ffmpeg with support compiled in
+        # TODO: What about AMD graphics card acceleration?
+    ]
+elif platform.system() == 'Darwin':
+    available_encoders = [
+        'vtenc_h264',
+        'vtenc_h264_hw'
+    ]
+elif platform.system() == 'Windows':
+    available_encoders = []
 
 class ScreenRecorder():
 
-    ENCODERS = [
-        'vaapi', # Intel CPU driver (only when running Xorg on Intel or Glamour drivers
-        'nvenc', # NVidia encoder, needs GTX680 or higher (GK104/Keppler or higher) and ffmpeg with support compiled in
-        'software' # Uses libx264 'veryfast' preset, needs much CPU power
-        # TODO: What about AMD graphics card acceleration?
-    ]
+    ENCODERS = available_encoders
 
-    def __init__(self, width=1920, height=1080, scale_width=None, scale_height=None, encoder='software'):
+    def __init__(self, width=1920, height=1080, scale_width=None, scale_height=None, encoder=None, display=0):
         if not encoder in ScreenRecorder.ENCODERS:
             raise NotImplementedError("Encoder '{}' not implemented".format(encoder))
 
@@ -27,18 +38,33 @@ class ScreenRecorder():
         self.scale_height = scale_height
         self.width = width
         self.height = height
-        self.encoder = encoder
+        self.encoder = encoder if encoder else ScreenRecorder.ENCODERS[0]
+        self.display = display
         self.build_gst_pipeline(encoder)
+        self.rfd = None
+        self.wfd = None
 
     def build_gst_pipeline(self, encoding_method):
         # display src
-        src = Gst.ElementFactory.make('ximagesrc', 'source')
-        src.set_property('display-name', ':0')
-        src.set_property('use-damage', 0)
-        src.set_property('startx', 0)
-        src.set_property('starty', 0)
-        src.set_property('endx', self.width - 1)
-        src.set_property('endy', self.height - 1)
+        src = None
+
+        if platform.system() == 'Linux':
+            src = Gst.ElementFactory.make('ximagesrc', 'source')
+            src.set_property('display-name', ':0.{}'.format(self.display))
+            src.set_property('use-damage', 0)
+            src.set_property('startx', 0)
+            src.set_property('starty', 0)
+            src.set_property('endx', self.width - 1)
+            src.set_property('endy', self.height - 1)
+        elif platform.system() == 'Darwin':
+            src = Gst.ElementFactory.make('avfvideosrc', 'source')
+            src.set_property('capture-screen', True)
+            src.set_property('capture-screen-cursor', True)
+            src.set_property('device-index', self.display)
+        elif platform.system() == 'Windows':
+            pass
+
+        # avfvideosrc capture-screen=true
 
         # assemble pipeline
         self.pipeline = Gst.Pipeline.new('playback')
@@ -80,6 +106,32 @@ class ScreenRecorder():
 
             encoder = Gst.ElementFactory.make('x264enc')
             encoder.set_property('speed-preset', 'veryfast')
+
+            if self.scale_width and self.scale_height:
+                scaler = Gst.ElementFactory.make('videoscale')
+                cap_string = 'video/x-raw,width={},height={}'.format(
+                    self.scale_width, self.scale_height
+                )
+                caps = Gst.Caps.from_string(cap_string)
+                filter = Gst.ElementFactory.make('capsfilter')
+                filter.set_property('caps', caps)
+
+                self.pipeline.add(scaler)
+                self.pipeline.add(filter)
+                self.pipeline.add(encoder)
+                convert.link(scaler)
+                scaler.link(filter)
+                filter.link(encoder)
+            else:
+                self.pipeline.add(encoder)
+                convert.link(encoder)
+        elif encoding_method == 'vtenc_h264' or encoding_method == 'vtenc_h264_hw':
+            # scale, convert and encode with software encoders
+            convert = Gst.ElementFactory.make('autovideoconvert')
+            self.pipeline.add(convert)
+            src.link(convert)
+
+            encoder = Gst.ElementFactory.make(encoding_method)
 
             if self.scale_width and self.scale_height:
                 scaler = Gst.ElementFactory.make('videoscale')
@@ -201,14 +253,19 @@ class ScreenRecorder():
         if self.wfd:
             os.close(self.wfd)
 
-def main(filename='~/capture.mkv', width=1920, height=1080, scale_width=None, scale_height=None, encoder='software'):
+def main(filename='~/capture.mkv', width=1920, height=1080, scale_width=None, scale_height=None, encoder=None, display=0):
     # Initialize Gstreamer
     GObject.threads_init()
     mainloop = GObject.MainLoop()
     Gst.init(None)
 
     # Start screen recorder
-    recorder = ScreenRecorder(width=width, height=height, scale_height=scale_height, scale_width=scale_width, encoder=encoder)
+    recorder = ScreenRecorder(width=width,
+                              height=height,
+                              scale_height=scale_height,
+                              scale_width=scale_width,
+                              encoder=encoder,
+                              display=display)
     recorder.start(path=filename)
 
     # run the main loop
@@ -221,8 +278,70 @@ def main(filename='~/capture.mkv', width=1920, height=1080, scale_width=None, sc
 
 # if run as script start recording
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print('Usage: {} <filename>'.format(sys.argv[0]))
-        exit(1)
+    parser = argparse.ArgumentParser(description='Record the screen')
+    parser.add_argument(
+        '-d', '--display',
+        type=int,
+        nargs=1,
+        default=0,
+        dest='display',
+        help='display index to capture'
+    )
+    parser.add_argument(
+        '-x', '--width',
+        type=int,
+        nargs=1,
+        default=1920,
+        dest='width',
+        help='width of the screen area to capture'
+    )
+    parser.add_argument(
+        '-y', '--height',
+        type=int,
+        nargs=1,
+        default=1080,
+        dest='height',
+        help='height of the screen area to capture'
+    )
+    parser.add_argument(
+        '--scaled-width',
+        type=int,
+        nargs=1,
+        default=None,
+        dest='scaled_width',
+        help='width of the recorded video file'
+    )
+    parser.add_argument(
+        '--scaled-height',
+        type=int,
+        nargs=1,
+        default=None,
+        dest='scaled_height',
+        help='height of the recorded video file'
+    )
+    parser.add_argument(
+        '-e', '--encoder',
+        type=str,
+        nargs=1,
+        dest='encoder',
+        default=ScreenRecorder.ENCODERS[0],
+        choices=ScreenRecorder.ENCODERS,
+        help='encoder to use'
+    )
+    parser.add_argument(
+        'filename',
+        nargs=1,
+        type=str,
+        help='output file'
+    )
+    args = parser.parse_args()
 
-    main(filename=sys.argv[1])
+    main(
+        filename=args.filename[0],
+        width=args.width,
+        height=args.height,
+        scale_width=args.scaled_width,
+        scale_height=args.scaled_height,
+        encoder=args.encoder[0],
+        display=args.display
+    )
